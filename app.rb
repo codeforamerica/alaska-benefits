0601dae426f135a345fb8ad865564a31cc0ddc7b
Andrew Hyder <hyd415@gmail.com>
2019-03-22 10:54:51 -0700

    Sinatra

---
+++ app.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+
+class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  get "/minimal.html" do
+    erb :minimal
+  end
+
+  get "/minimal" do
+    erb :minimal
+  end
+end
