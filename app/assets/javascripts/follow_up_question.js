ad5862ce603986ba2f0eb660a148e3f0c06cf510
Andrew Hyder <hyd415@gmail.com>
2019-04-23 15:45:11 -0700

    Clean up js

--- app/assets/javascripts/follow_up_question.js
+++ app/assets/javascripts/follow_up_question.js
@@ -1,10 +1,13 @@
 var followUpQuestion = (function() {
   var fUQ = {
     init: function() {
+      // The builder only applies styles to underlying elements, while we want to hide the whole form group.
       $('.question-with-follow-up__follow-up .form-group').hide();
 
       // add click listeners to initial question inputs
       $('.question-with-follow-up__question').click(function(e) {
+
+        // show the follow up if there are any checkboxes clicked
         if ($("input:checkbox:checked").length > 0)
         {
           $('.question-with-follow-up__follow-up').show();
@@ -14,6 +17,7 @@ var followUpQuestion = (function() {
           $('.question-with-follow-up__follow-up').hide();
         }
 
+        // show and hide the follow up inputs
         data_target = e.target.attributes["follow-up-target"]["value"];
         selector = "*[follow-up='" + data_target + "']";
         if (e.target["checked"]) {
@@ -21,11 +25,9 @@ var followUpQuestion = (function() {
         } else {
           $(selector).parent().hide();
         }
-
-
       });
     }
-  }
+  };
   return {
     init: fUQ.init
   }
