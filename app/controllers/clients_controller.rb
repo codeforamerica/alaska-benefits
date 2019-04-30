f9a9f76473459656d08d8d25bee0c8af321716a5
Andrew Hyder <hyd415@gmail.com>
2019-04-30 16:31:00 -0700

    Protect feedback

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,6 +2,8 @@ class ClientsController < ApplicationController
   # GET /clients/1
   def show
     @client = Client.find params[:id]
+
+    redirect_to root_path unless current_client == @client
     @feedback = Feedback.new
   end
 
@@ -19,6 +21,7 @@ class ClientsController < ApplicationController
     @client = Client.new(client_params)
 
     if @client.save
+      session[:current_client_id] = @client.id
       redirect_to @client, notice: 'Client was successfully created.'
     else
       render :new
