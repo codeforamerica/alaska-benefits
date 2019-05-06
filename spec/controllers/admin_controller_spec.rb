2223284a5bb7cf3274596eb5ec7c763deb0a99db
Andrew Hyder <hyd415@gmail.com>
2019-05-06 10:47:01 -0700

    mailing pdf with lob spec

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe AdminController do
       expect(response.headers.to_h).to include "Content-Type" => "application/pdf"
     end
   end
+
+  describe "mailing pdfs" do
+    it "works" do
+
+      lob_spy = instance_double(Lob::Client)
+      allow(Lob::Client).to receive(:new).and_return(lob_spy)
+      allow(lob_spy).to receive_message_chain(:letters, :create).and_return 200
+
+      sign_in create(:admin)
+      client = create :client
+
+      post :mail_pdf, params: { id: client.id }, format: 'js'
+
+
+      expect(lob_spy).to have_received(:letters)
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
