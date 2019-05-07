b1fee1962b3e7d06c9e083862a8ba8ae644ce2c4
Andrew Hyder <hyd415@gmail.com>
2019-05-07 13:56:21 -0700

    Can now add the clients address to be able to use it as the return address when mailing.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,22 +1,12 @@
 class ClientsController < ApplicationController
-  # GET /clients/1
-  def show
-    @client = Client.find params[:id]
-
-    redirect_to root_path unless current_client == @client
-    @feedback = Feedback.new
-  end
-
-  # GET /clients/new
-  def new
-    @client = Client.new
-  end
-
   def index
     redirect_to "/upload"
   end
 
-  # POST /clients
+  def new
+    @client = Client.new
+  end
+
   def create
     @client = Client.new(client_params)
 
@@ -28,10 +18,24 @@ class ClientsController < ApplicationController
     end
   end
 
+  def update
+    @client = Client.find params[:id]
+    @client.update client_params
+
+    redirect_to "/admin"
+  end
+
+  def show
+    @client = Client.find params[:id]
+
+    redirect_to root_path unless current_client == @client
+    @feedback = Feedback.new
+  end
+
   private
 
   # Only allow a trusted parameter "white list" through.
   def client_params
-    params.require(:client).permit(:phone, :text_opt_in, :email, :email_opt_in, :office_choice, :document)
+    params.require(:client).permit(:phone, :text_opt_in, :email, :email_opt_in, :office_choice, :document, :name, :street_address, :city, :state, :zip_code)
   end
 end
--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe AdminController do
 
   describe "#mail_pdf" do
     it "works" do
-
       lob_mock = instance_double(Lob::Client)
       allow(Lob::Client).to receive(:new).and_return(lob_mock)
       allow(lob_mock).to receive_message_chain(:letters, :create).and_return 200
@@ -22,10 +21,9 @@ RSpec.describe AdminController do
       sign_in create(:admin)
       client = create :client
       
-      post :mail_pdf, params: { id: client.id }, format: 'js'
+      post :mail_pdf, params: { id: client.id }
 
       expect(lob_mock).to have_received(:letters)
-      expect(response).to have_http_status(:success)
       expect(response).to redirect_to "/admin"
     end
   end
