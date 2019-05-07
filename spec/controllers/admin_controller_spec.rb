fd792341a975b719a9a72f73f4ae7f0c14c04dd6
Andrew Hyder <hyd415@gmail.com>
2019-05-07 11:22:35 -0700

    Better specs

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe AdminController do
-  describe "downloading pdfs" do
+  describe "#pdf" do
     it "works" do
       sign_in create(:admin)
 
@@ -12,20 +12,19 @@ RSpec.describe AdminController do
     end
   end
 
-  describe "mailing pdfs" do
+  describe "#mail_pdf" do
     it "works" do
 
-      lob_spy = instance_double(Lob::Client)
-      allow(Lob::Client).to receive(:new).and_return(lob_spy)
-      allow(lob_spy).to receive_message_chain(:letters, :create).and_return 200
+      lob_mock = instance_double(Lob::Client)
+      allow(Lob::Client).to receive(:new).and_return(lob_mock)
+      allow(lob_mock).to receive_message_chain(:letters, :create).and_return 200
 
       sign_in create(:admin)
       client = create :client
-
+      
       post :mail_pdf, params: { id: client.id }, format: 'js'
 
-
-      expect(lob_spy).to have_received(:letters)
+      expect(lob_mock).to have_received(:letters)
       expect(response).to have_http_status(:success)
     end
   end
--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature "Admin", type: :feature do
+  before do
+    login_as create(:admin)
+    lob_mock = instance_double(Lob::Client)
+    allow(Lob::Client).to receive(:new).and_return(lob_mock)
+    allow(lob_mock).to receive_message_chain(:letters, :create).and_return 200
+  end
+
   scenario "Admins can download client's pdf" do
-    admin = create :admin
-    login_as admin
-
     create :client
-
     visit "/admin"
-
     expect(page).to have_content "Application Link"
+  end
+
+  scenario "Admins can mail the client's application" do
+    create :client
+    visit "/admin"
+    accept_alert "OKAY" do
+      click_on "Mail Application to State"
+    end
   end
 end
